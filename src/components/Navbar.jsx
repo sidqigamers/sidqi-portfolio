00b5932b56f25ae4e12efc4973e5442929f58191
sidqi <sidqigamers@gmail.com>
2025-12-28 20:10:05 +0700

    Navbar scrollable (for fold 5, and other small width devices)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from 'react'
+import darkIcon from '../assets/dark-mode.png'
+import lightIcon from '../assets/light-mode.png'
+
 function Navbar() {
+  const [isDark, setIsDark] = useState(false)
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add('dark')
+    } else {
+      document.body.classList.remove('dark')
+    }
+  }, [isDark])
+
+  const toggleDarkMode = () => {
+    setIsDark(!isDark)
+  }
+
   return (
     <nav className="navbar">
       <div className="nav-logo">Sidqi</div>
@@ -7,9 +25,20 @@ function Navbar() {
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
+        <li>
+          <button
+              className="dark-toggle"
+              onClick={toggleDarkMode}
+              ria-label="Toggle Dark Mode">
+                <img
+                src={isDark ? lightIcon : darkIcon}
+                alt="Toggle theme"
+                className="dark-icon"/>
+          </button>
+          </li>
       </ul>
     </nav>
-  ) 
+  )
 }
 
 export default Navbar
